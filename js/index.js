3052939e7707ce5dbde409045721929dffd9208c
Михаил Лагузов <78651970+Nerian4ik@users.noreply.github.com>
2023-07-05 18:01:27 +0300

    v2.0

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.getElementById('brand').addEventListener('change', function() {
 
     var models;
     if (brand === 'mazda') {
-        models = ['3', '6', 'CX-5', 'CX-9'];
+        models = ['Mazda3', 'Mazda6', 'CX-5', 'CX-9'];
     } else if (brand === 'ford') {
         models = ['Fiesta', 'Focus', 'Mustang', 'Explorer'];
     }
@@ -17,12 +17,46 @@ document.getElementById('brand').addEventListener('change', function() {
         modelSelect.appendChild(option);
     }
 
-    // Показать выбор модели и типа кузова после выбора марки
-    modelSelect.classList.remove('hidden');
-    document.getElementById('bodyType').classList.remove('hidden');
+    // Показать выбор модели после выбора марки
+    modelSelect.classList.remove('d-none');
 });
 
-document.getElementById('bodyType').addEventListener('click', function() {
-    // Показать результаты после выбора типа кузова
-    document.getElementById('results').classList.remove('hidden');
+document.getElementById('model').addEventListener('change', function() {
+    // Показать выбор типа кузова после выбора модели
+    document.getElementById('bodyType').classList.remove('d-none');
+});
+
+document.getElementById('bodyType').addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        // Снимаем активность со всех кнопок
+        var buttons = this.getElementsByTagName('button');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('active');
+        }
+        // Делаем активной нажатую кнопку
+        event.target.classList.add('active');
+        // Показать зоны обработки и выбор размера листа после выбора типа кузова
+        document.getElementById('processingZones').classList.remove('d-none');
+        document.getElementById('sheetSize').classList.remove('d-none');
+    }
+});
+
+document.getElementById('processingZones').addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        event.target.classList.toggle('active');
+    }
+});
+
+document.getElementById('sheetSize').addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        // Снимаем активность со всех кнопок
+        var buttons = this.getElementsByTagName('button');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('active');
+        }
+        // Делаем активной нажатую кнопку
+        event.target.classList.add('active');
+        // Показать выбор качества после выбора размера листа
+        document.getElementById('quality').classList.remove('d-none');
+    }
 });
